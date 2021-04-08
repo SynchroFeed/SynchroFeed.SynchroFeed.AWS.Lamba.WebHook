a65621d290aef1c2c4d3a55e9f005f3e83a62237
Andrew Rissing <arissing@alkamitech.com>
2021-04-08 09:05:33 -0500

    Updated packages. Corrected failing unit test. Streamlined WebMethod logic.

--- test/WebHookPostTest.cs
+++ test/WebHookPostTest.cs
@@ -1,20 +1,21 @@
 #region header
+
 // --------------------------------------------------------------------------------------------------------------------
 // <copyright company="Robert Vandehey" file="WebHookPostTest.cs">
 // MIT License
-// 
+//
 // Copyright(c) 2018 Robert Vandehey
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,16 +25,19 @@
 // SOFTWARE.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
-#endregion
-using System;
-using System.Net;
-using System.Threading;
+
+#endregion header
+
 using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
 using Amazon.SimpleNotificationService;
 using Amazon.SimpleNotificationService.Model;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Moq;
+using System;
+using System.Net;
+using System.Threading;
 using Xunit;
 
 namespace SynchroFeed.AWS.Lambda.WebHook.Test
@@ -64,6 +68,8 @@ namespace SynchroFeed.AWS.Lambda.WebHook.Test
             LoggerMock.Setup(a => a.LogLine(It.IsAny<string>()));
             ContextMock = new Mock<ILambdaContext>();
             ContextMock.Setup(a => a.Logger).Returns(LoggerMock.Object);
+
+            WebMethods.GetSnsClientFunc = credentials => SnsMock.Object;
         }
 
         [Fact]
@@ -74,10 +80,9 @@ namespace SynchroFeed.AWS.Lambda.WebHook.Test
             Environment.SetEnvironmentVariable("WEBHOOK_AWSCREDENTIALS:SECRETKEY", "TestSecretKey");
             Environment.SetEnvironmentVariable("WEBHOOK_AWSSNS:TOPIC", "TestSnsTopic");
 
-            WebMethods = new WebMethods((context, builder) => builder.AddEnvironmentVariables("WEBHOOK_"))
-            {
-                GetSnsClientFunc = credentials => SnsMock.Object
-            };
+            WebMethods.ConfigurationBuilderFunc = (context, builder) => builder.AddEnvironmentVariables("WEBHOOK_");
+
+            WebMethods = new WebMethods();
 
             Assert.Equal("TestAccessKey", WebMethods.awsCredentials.AccessKey);
             Assert.Equal("TestSecretKey", WebMethods.awsCredentials.SecretKey);
@@ -91,25 +96,36 @@ namespace SynchroFeed.AWS.Lambda.WebHook.Test
         [Fact]
         public void Test_WebMethod_Post_With_Json_Config()
         {
-            WebMethods = new WebMethods((context, builder) => builder.AddJsonFile("testconfig.json", false))
-            {
-                GetSnsClientFunc = credentials => SnsMock.Object
-            };
+            WebMethods.ConfigurationBuilderFunc = (context, builder) => builder.AddJsonFile("testconfig.json", false);
+
+            WebMethods = new WebMethods();
 
             Assert.Equal("TESTACCESSKEY", WebMethods.awsCredentials.AccessKey);
             Assert.Equal("TESTSECRETKEY", WebMethods.awsCredentials.SecretKey);
             Assert.Equal("arn:aws:sns:us-east-1:TEST:TestFeed", WebMethods.awsSns.Topic);
 
             var response = WebMethods.Post(RequestMock.Object, ContextMock.Object);
-            Assert.Equal((int) HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal((int)HttpStatusCode.OK, response.StatusCode);
             SnsMock.Verify();
         }
 
         [Fact]
         public void Test_WebMethod_Default_Create()
         {
+            var called = false;
+
             Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", null);
+
+            WebMethods.ConfigurationBuilderFunc = (hostingContext, builder) =>
+            {
+                called = true;
+                Assert.True(hostingContext.HostingEnvironment.IsDevelopment());
+                return builder;
+            };
+
             WebMethods = new WebMethods();
+
+            Assert.True(called);
         }
 
         // ReSharper disable once ParameterOnlyUsedForPreconditionCheck.Local
